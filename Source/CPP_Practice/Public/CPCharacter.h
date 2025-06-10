2a826e9806d961c1def3e5ea553ccce0be82fd13
ShinoharaGauze <1550326266@qq.com>
2025-06-10 17:30:24 +0800

    Added CPCharacter & Setup Input
    - Added Character class
    - Added MoveForward/Turn input

---
+++ Source/CPP_Practice/Public/CPCharacter.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CPCharacter.generated.h"
+
+
+
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class CPP_PRACTICE_API ACPCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACPCharacter();
+
+protected:
+
+	UPROPERTY(visibleAnywhere, Category = "Component")
+	TObjectPtr<USpringArmComponent> SpringArmComp;
+
+	UPROPERTY(visibleAnywhere)
+	UCameraComponent* CameraComp;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Value);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	
+};
