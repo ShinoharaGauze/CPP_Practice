4eadfcf7dacb48d92e23e49ea7a95ec7b2003df6
ShinoharaGauze <1550326266@qq.com>
2025-06-11 18:30:07 +0800

    Interface
    - Added Interface

---
+++ Source/CPP_Practice/Private/CPGameplayInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPGameplayInterface.h"
+
+// Add default functionality here for any ICPGameplayInterface functions that are not pure virtual.
